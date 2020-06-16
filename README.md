6691bf58b1efe459b3ba5c270721d5f1f4df7cb1
unknown <ntsh.vicky@gmail.com>
2020-06-17 00:13:27 +0530

    yolo 2

---
+++ README.md
@@ -0,0 +1,17 @@
+# yolo-image-differences
+Find the missing object in two different images using Yolo V3 and Python and OpenCV.
+
+This file help you to find out missing object from two same but small diiferences based images. 
+
+# Requieremnts
+Python 3.5+
+OpenCV
+Numpy
+
+# Execute using this command (python 3 user use python3) 
+python yolo.py -i1 .\images\dining_table.jpg -i2 .\images\dining_table2.jpg -y .\yolo-coco\
+
+# Download Yolo Folder's File (Weight,cfg and name)
+https://drive.google.com/file/d/1ANEk5nYFxP3aBUV48cPhTXNgwff_RhKw/view?usp=sharing
+
+
---
+++ yolo.py
@@ -0,0 +1,166 @@
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+
+import numpy as np
+import time
+import os
+from PIL import Image
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i1", "--image1", required=True,
+	help="path to input image")
+ap.add_argument("-i2", "--image2", required=True,
+	help="path to input image")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+# load the two input images
+imageA = cv2.imread(args['image1'])
+imageB = cv2.imread(args['image2'])
+
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+crops = []
+# loop over the contours
+for c in cnts:
+    # compute the bounding box of the contour and then draw the
+    # bounding box on both input images to represent where the two
+    # images differ
+    (x, y, w, h) = cv2.boundingRect(c)
+    cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    crops.append(imageB[y : y + h, x : x + w])
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+
+objectname = []
+for crop in crops:
+    # load our input image and grab its spatial dimensions
+    image = crop
+    (H, W) = image.shape[:2]
+
+    # determine only the *output* layer names that we need from YOLO
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    # construct a blob from the input image and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes and
+    # associated probabilities
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+        swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    # show timing information on YOLO
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    # initialize our lists of detected bounding boxes, confidences, and
+    # class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > args["confidence"]:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+        args["threshold"])
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # draw a bounding box rectangle and label on the image
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            objectname.append((LABELS[classIDs[i]], confidences[i]))
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, color, 2)
+
+print("missing objects is :")
+print(objectname)
+# show the output images
+cv2.imshow("image1.jpg", imageA)
+cv2.imshow("image2.jpg", imageB)
+cv2.imshow("Diff.jpg", crop)
+#cv2.imshow("Thresh.jpg", thresh)
+cv2.waitKey(0)
